ac60dd3c957756ecfbb340d95c8c9949263a8138
Ponomareva Nina <nina.9412@mail.ru>
2023-06-15 18:28:54 +0300

    feat: add store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./variables.scss";
 import "./index.scss";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>
 );
