f3aab531ab0fd8202cf4c23010c7c6c1e8cf6be3
Ponomareva Nina <nina.9412@mail.ru>
2023-06-17 18:17:25 +0300

    feat: add custom icon to stepper

--- src/components/MultiStepForm/Stepper/Stepper.tsx
+++ src/components/MultiStepForm/Stepper/Stepper.tsx
@@ -3,6 +3,7 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import { FC, useId } from "react";
 import { StepLabel } from "@mui/material";
+import icon from "../../../assets/done_icon.svg";
 
 interface StepperProps {
   currentStep: number;
@@ -19,14 +20,27 @@ export const MultiStepper: FC<StepperProps> = ({ currentStep, stepsLength }) => 
         fill: "none",
       },
     },
+    "& .icon": {
+      height: "2rem",
+      width: "2rem",
+      background: "#A6A6A6",
+      borderRadius: "50%",
+    },
     "& .Mui-active": {
       "& .MuiStepConnector-line": {
         borderColor: "#5558FA",
+      },
+      "& .icon": {
+        background:
+          "radial-gradient(circle at center, white 0, white 2px, #5558FA 3px, #5558FA 100%)",
       },
     },
     "& .Mui-completed": {
       "& .MuiStepConnector-line": {
         borderColor: "#5558FA",
+      },
+      "& .icon": {
+        background: `#5558FA url('${icon}') no-repeat center`,
       },
     },
     "& .MuiStepConnector-root": {
@@ -45,7 +59,7 @@ export const MultiStepper: FC<StepperProps> = ({ currentStep, stepsLength }) => 
         const isCompleted = currentStep > index;
         return (
           <Step key={useId()} completed={isCompleted}>
-            <StepLabel>{index + 1}</StepLabel>
+            <StepLabel icon={<div className="icon" />}>{index + 1}</StepLabel>
           </Step>
         );
       })}
