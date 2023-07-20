7b5d8ed15261649201098dd0ffc3c47d1438df05
Нина Пономарева <nina.9412@mail.ru>
2023-07-20 23:56:25 +0300

    Update mock-user.ts

--- src/mock-data/mock-user.ts
+++ src/mock-data/mock-user.ts
@@ -1,13 +1,13 @@
 import { User } from "../interfaces/user.interface";
 
 export const mockUser: User = {
-  phone: "+79110047229",
-  email: "nina.9412@mail.ru",
+  phone: "+88123456789",
+  email: "test@mail.ru",
   name: "Нина",
   surname: "Пономарева",
   socials: {
-    telegram: "https://t.me/TypicallyNi",
+    telegram: "",
     github: "https://github.com/Ponomareva-Nina",
-    resume: "https://hh.ru/resume/ab08a8e2ff09723f480039ed1f41694f457779",
+    resume: "",
   },
 };
